d1669a8e526caf8cc8e4324aef4de368a4294683
Chirag <chrgsoni07@gmail.com>
2021-04-13 18:27:59 +0530

    changes related to dialog of prime ng

--- src/component/Items/index.tsx
+++ src/component/Items/index.tsx
@@ -1,16 +1,25 @@
 import { FC } from "react";
 import { Button } from "primereact/button";
-import AddItemModal from "./AddItemModal";
 import { Column } from "primereact/column";
 import { Toolbar } from "primereact/toolbar";
-import { Card } from "primereact/card";
 import { DataTable } from "primereact/datatable";
 import { TabView, TabPanel } from "primereact/tabview";
 import React, { useState } from "react";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+import classNames from "classnames";
 
 const Items = () => {
+  let emptyGoldItem = {
+    id: null,
+    name: "",
+  };
+
   const [modalShow, setModalShow] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
+  const [goldItem, setGoldItem] = useState(emptyGoldItem);
+  const [submitted, setSubmitted] = useState(false);
+  const [newGoldItemDialog, setNewGoldItemDialog] = useState(false);
 
   const goldItems = [
     { id: "1", name: "Jhumki" },
@@ -64,11 +73,58 @@ const Items = () => {
 
   const confirmDeleteSelected = () => {};
 
-  const openNew = () => {};
+  const openNew = () => {
+    setGoldItem(emptyGoldItem);
+    setSubmitted(false);
+    setNewGoldItemDialog(true);
+  };
 
   const editProduct = (rowData: any) => {};
 
   const confirmDeleteProduct = (rowData: any) => {};
+
+  const saveNewGoldItem = () => {
+    setSubmitted(true);
+  };
+
+  const hideDialog = () => {
+    setSubmitted(false);
+    setNewGoldItemDialog(false);
+  };
+
+  const goldItemDialogFooter = (
+    <React.Fragment>
+      <Button
+        label="Cancel"
+        icon="pi pi-times"
+        className="p-button-text"
+        onClick={hideDialog}
+      />
+      <Button
+        label="Save"
+        icon="pi pi-check"
+        className="p-button-text"
+        onClick={saveNewGoldItem}
+      />
+    </React.Fragment>
+  );
+
+  const productDialogFooter = (
+    <React.Fragment>
+      <Button
+        label="Cancel"
+        icon="pi pi-times"
+        className="p-button-text"
+        onClick={hideDialog}
+      />
+      <Button
+        label="Save"
+        icon="pi pi-check"
+        className="p-button-text"
+        onClick={saveNewGoldItem}
+      />
+    </React.Fragment>
+  );
 
   return (
     <>
@@ -89,11 +145,38 @@ const Items = () => {
                 selectionMode="single"
                 dataKey="id"
               >
-                <Column field="id" header="Id"></Column>
-                <Column field="name" header="Name"></Column>
+                <Column field="id" header="Id" sortable></Column>
+                <Column field="name" header="Name" sortable></Column>
                 <Column body={actionBodyTemplate}></Column>
               </DataTable>
             </div>
+            {/* dialog to add new item*/}
+
+            <Dialog
+              header="Add new gold item"
+              visible={newGoldItemDialog}
+              style={{ width: "450px" }}
+              modal
+              className="p-fluid"
+              footer={productDialogFooter}
+              onHide={hideDialog}
+            >
+              <div className="p-field">
+                <label htmlFor="name">Name</label>
+                <InputText
+                  id="name"
+                  value={goldItem.name}
+                  required
+                  autoFocus
+                  className={classNames({
+                    "p-invalid": submitted && !goldItem.name,
+                  })}
+                />
+                {submitted && !goldItem.name && (
+                  <small className="p-error">Name is required.</small>
+                )}
+              </div>
+            </Dialog>
           </TabPanel>
 
           <TabPanel header="Silver per price items">
@@ -111,32 +194,38 @@ const Items = () => {
                 selectionMode="single"
                 dataKey="id"
               >
-                <Column field="id" header="Id"></Column>
-                <Column field="name" header="Name"></Column>
+                <Column field="id" header="Id" sortable></Column>
+                <Column field="name" header="Name" sortable></Column>
                 <Column body={actionBodyTemplate}></Column>
               </DataTable>
             </div>
           </TabPanel>
 
           <TabPanel header="Silver per weight items">
-            <div className="card">
-              <Toolbar className="p-mb-4" left={leftToolbarTemplate}></Toolbar>
-              <DataTable
-                value={silverPerWeightItems}
-                selection={selectedProduct}
-                onSelectionChange={(e) => setSelectedProduct(e.value)}
-                paginator
-                rows={10}
-                rowsPerPageOptions={[5, 10, 25]}
-                paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-                currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
-                selectionMode="single"
-                dataKey="id"
-              >
-                <Column field="id" header="Id"></Column>
-                <Column field="name" header="Name"></Column>
-                <Column body={actionBodyTemplate}></Column>
-              </DataTable>
+            <div className="datatable-responsive-demo">
+              <div className="card">
+                <Toolbar
+                  className="p-mb-4"
+                  left={leftToolbarTemplate}
+                ></Toolbar>
+                <DataTable
+                  value={silverPerWeightItems}
+                  selection={selectedProduct}
+                  onSelectionChange={(e) => setSelectedProduct(e.value)}
+                  paginator
+                  rows={10}
+                  className="p-datatable-responsive-demo"
+                  rowsPerPageOptions={[5, 10, 25]}
+                  paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                  currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+                  selectionMode="single"
+                  dataKey="id"
+                >
+                  <Column field="id" header="Id" sortable></Column>
+                  <Column field="name" header="Name" sortable></Column>
+                  <Column body={actionBodyTemplate}></Column>
+                </DataTable>
+              </div>
             </div>
           </TabPanel>
         </TabView>
