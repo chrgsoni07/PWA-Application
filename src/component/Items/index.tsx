1c3f9f05f2aa05f7c9c48af29a7e9b9fe443cb4b
Ankit Soni <ankitprahladsoni@gmail.com>
2021-04-13 22:11:57 -0700

    refactor

--- src/component/Items/index.tsx
+++ src/component/Items/index.tsx
@@ -1,49 +1,74 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Button } from "primereact/button";
 import { Column } from "primereact/column";
 import { Toolbar } from "primereact/toolbar";
 import { DataTable } from "primereact/datatable";
 import { TabView, TabPanel } from "primereact/tabview";
-import React, { useState } from "react";
 import { Dialog } from "primereact/dialog";
 import { InputText } from "primereact/inputtext";
 import classNames from "classnames";
+import {
+  goldItems as mockGoldItems,
+  silverPerPriceItems as mockSilverPerPriceItems,
+  silverPerWeightItems as mockSilverPerWeightItems,
+} from "./mockData";
 
 const Items = () => {
-  let emptyGoldItem = {
-    id: null,
-    name: "",
-  };
+  const confirmDeleteSelected = () => {};
 
-  const [modalShow, setModalShow] = useState(false);
+  const [goldItems, setGoldItems] = useState(mockGoldItems);
+  const [silverPerPriceItems, setSilverPerPriceItems] = useState(
+    mockSilverPerPriceItems
+  );
+  const [silverPerWeightItems, setSilverPerWeightItems] = useState(
+    mockSilverPerWeightItems
+  );
+
+  return (
+    <>
+      <div className="card">
+        <TabView>
+          <TabPanel header="Gold Items">
+            <ItemsPanel items={goldItems} updateItems={setGoldItems} />
+          </TabPanel>
+          <TabPanel header="Silver per price items">
+            <ItemsPanel
+              items={silverPerPriceItems}
+              updateItems={setSilverPerPriceItems}
+            />
+          </TabPanel>
+          <TabPanel header="Silver per weight items">
+            <ItemsPanel
+              items={silverPerWeightItems}
+              updateItems={setSilverPerWeightItems}
+            />
+          </TabPanel>
+        </TabView>
+      </div>
+    </>
+  );
+};
+
+export default Items;
+
+type ItemType = { id: string; name: string };
+type Props = {
+  items: ItemType[];
+  updateItems: (items: ItemType[]) => void;
+};
+const ItemsPanel: FC<Props> = ({ items, updateItems }) => {
   const [selectedProduct, setSelectedProduct] = useState(null);
-  const [goldItem, setGoldItem] = useState(emptyGoldItem);
+  const [goldItem, setGoldItem] = useState("");
   const [submitted, setSubmitted] = useState(false);
   const [newGoldItemDialog, setNewGoldItemDialog] = useState(false);
-
-  const goldItems = [
-    { id: "1", name: "Jhumki" },
-    { id: "2", name: "Haar" },
-    { id: "3", name: "Mangal Sutra" },
-    { id: "4", name: "Anguthi" },
-  ];
-
-  const silverPerPriceItems = [
-    { id: "1", name: "Anguthi" },
-    { id: "2", name: "Pendil" },
-    { id: "3", name: "Chandrama" },
-  ];
-
-  const silverPerWeightItems = [
-    { id: "1", name: "Payjab" },
-    { id: "2", name: "Bicchi" },
-    { id: "3", name: "Chain" },
-    { id: "4", name: "Baccha ka kada" },
-  ];
+  const editProduct = (rowData: any) => {
+    console.log(rowData);
+  };
+  const confirmDeleteProduct = (rowData: any) => {};
 
   const actionBodyTemplate = (rowData: any) => {
     return (
-      <React.Fragment>
+      <>
         <Button
           icon="pi pi-pencil"
           className="p-button-rounded p-button-success p-mr-2"
@@ -54,35 +79,28 @@ const Items = () => {
           className="p-button-rounded p-button-warning"
           onClick={() => confirmDeleteProduct(rowData)}
         />
-      </React.Fragment>
+      </>
     );
+  };
+
+  const openNew = () => {
+    setGoldItem("");
+    setSubmitted(false);
+    setNewGoldItemDialog(true);
   };
 
   const leftToolbarTemplate = () => {
     return (
-      <React.Fragment>
+      <>
         <Button
           label="New"
           icon="pi pi-plus"
           className="p-button-success p-mr-2"
           onClick={openNew}
         />
-      </React.Fragment>
+      </>
     );
   };
-
-  const confirmDeleteSelected = () => {};
-
-  const openNew = () => {
-    setGoldItem(emptyGoldItem);
-    setSubmitted(false);
-    setNewGoldItemDialog(true);
-  };
-
-  const editProduct = (rowData: any) => {};
-
-  const confirmDeleteProduct = (rowData: any) => {};
-
   const saveNewGoldItem = () => {
     setSubmitted(true);
   };
@@ -92,25 +110,8 @@ const Items = () => {
     setNewGoldItemDialog(false);
   };
 
-  const goldItemDialogFooter = (
-    <React.Fragment>
-      <Button
-        label="Cancel"
-        icon="pi pi-times"
-        className="p-button-text"
-        onClick={hideDialog}
-      />
-      <Button
-        label="Save"
-        icon="pi pi-check"
-        className="p-button-text"
-        onClick={saveNewGoldItem}
-      />
-    </React.Fragment>
-  );
-
   const productDialogFooter = (
-    <React.Fragment>
+    <>
       <Button
         label="Cancel"
         icon="pi pi-times"
@@ -123,115 +124,55 @@ const Items = () => {
         className="p-button-text"
         onClick={saveNewGoldItem}
       />
-    </React.Fragment>
+    </>
   );
 
   return (
     <>
       <div className="card">
-        <TabView>
-          <TabPanel header="Gold Items">
-            <div className="card">
-              <Toolbar className="p-mb-4" left={leftToolbarTemplate}></Toolbar>
-              <DataTable
-                value={goldItems}
-                selection={selectedProduct}
-                onSelectionChange={(e) => setSelectedProduct(e.value)}
-                paginator
-                rows={10}
-                rowsPerPageOptions={[5, 10, 25]}
-                paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-                currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
-                selectionMode="single"
-                dataKey="id"
-              >
-                <Column field="id" header="Id" sortable></Column>
-                <Column field="name" header="Name" sortable></Column>
-                <Column body={actionBodyTemplate}></Column>
-              </DataTable>
-            </div>
-            {/* dialog to add new item*/}
-
-            <Dialog
-              header="Add new gold item"
-              visible={newGoldItemDialog}
-              style={{ width: "450px" }}
-              modal
-              className="p-fluid"
-              footer={productDialogFooter}
-              onHide={hideDialog}
-            >
-              <div className="p-field">
-                <label htmlFor="name">Name</label>
-                <InputText
-                  id="name"
-                  value={goldItem.name}
-                  required
-                  autoFocus
-                  className={classNames({
-                    "p-invalid": submitted && !goldItem.name,
-                  })}
-                />
-                {submitted && !goldItem.name && (
-                  <small className="p-error">Name is required.</small>
-                )}
-              </div>
-            </Dialog>
-          </TabPanel>
-
-          <TabPanel header="Silver per price items">
-            <div className="card">
-              <Toolbar className="p-mb-4" left={leftToolbarTemplate}></Toolbar>
-              <DataTable
-                value={silverPerPriceItems}
-                selection={selectedProduct}
-                onSelectionChange={(e) => setSelectedProduct(e.value)}
-                paginator
-                rows={10}
-                rowsPerPageOptions={[5, 10, 25]}
-                paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-                currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
-                selectionMode="single"
-                dataKey="id"
-              >
-                <Column field="id" header="Id" sortable></Column>
-                <Column field="name" header="Name" sortable></Column>
-                <Column body={actionBodyTemplate}></Column>
-              </DataTable>
-            </div>
-          </TabPanel>
-
-          <TabPanel header="Silver per weight items">
-            <div className="datatable-responsive-demo">
-              <div className="card">
-                <Toolbar
-                  className="p-mb-4"
-                  left={leftToolbarTemplate}
-                ></Toolbar>
-                <DataTable
-                  value={silverPerWeightItems}
-                  selection={selectedProduct}
-                  onSelectionChange={(e) => setSelectedProduct(e.value)}
-                  paginator
-                  rows={10}
-                  className="p-datatable-responsive-demo"
-                  rowsPerPageOptions={[5, 10, 25]}
-                  paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-                  currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
-                  selectionMode="single"
-                  dataKey="id"
-                >
-                  <Column field="id" header="Id" sortable></Column>
-                  <Column field="name" header="Name" sortable></Column>
-                  <Column body={actionBodyTemplate}></Column>
-                </DataTable>
-              </div>
-            </div>
-          </TabPanel>
-        </TabView>
+        <Toolbar className="p-mb-4" left={leftToolbarTemplate}></Toolbar>
+        <DataTable
+          value={items}
+          selection={selectedProduct}
+          onSelectionChange={(e) => setSelectedProduct(e.value)}
+          paginator
+          rows={10}
+          rowsPerPageOptions={[5, 10, 25]}
+          paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+          currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+          selectionMode="single"
+          dataKey="id"
+        >
+          <Column field="id" header="Id" sortable></Column>
+          <Column field="name" header="Name" sortable></Column>
+          <Column body={actionBodyTemplate}></Column>
+        </DataTable>
       </div>
+      <Dialog
+        header="Add new gold item"
+        visible={newGoldItemDialog}
+        style={{ width: "450px" }}
+        modal
+        className="p-fluid"
+        footer={productDialogFooter}
+        onHide={hideDialog}
+      >
+        <div className="p-field">
+          <label htmlFor="name">Name</label>
+          <InputText
+            id="name"
+            value={goldItem}
+            required
+            autoFocus
+            className={classNames({
+              "p-invalid": submitted && !goldItem,
+            })}
+          />
+          {submitted && !goldItem && (
+            <small className="p-error">Name is required.</small>
+          )}
+        </div>
+      </Dialog>
     </>
   );
 };
-
-export default Items;
---
+++ src/component/Items/mockData.ts
@@ -0,0 +1,19 @@
+export const goldItems = [
+  { id: "1", name: "Jhumki" },
+  { id: "2", name: "Haar" },
+  { id: "3", name: "Mangal Sutra" },
+  { id: "4", name: "Anguthi" },
+];
+
+export const silverPerPriceItems = [
+  { id: "1", name: "Anguthi" },
+  { id: "2", name: "Pendil" },
+  { id: "3", name: "Chandrama" },
+];
+
+export const silverPerWeightItems = [
+  { id: "1", name: "Payjab" },
+  { id: "2", name: "Bicchi" },
+  { id: "3", name: "Chain" },
+  { id: "4", name: "Baccha ka kada" },
+];
