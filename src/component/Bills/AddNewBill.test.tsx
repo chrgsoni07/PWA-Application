9088611d90b346a14c22550525db0ea6929ed407
Ankit Soni <ankitprahladsoni@gmail.com>
2021-05-23 22:03:26 -0700

    edit a row test

--- src/component/Bills/AddNewBill.test.tsx
+++ src/component/Bills/AddNewBill.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, screen, within } from "@testing-library/react";
+import { logRoles, render, screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { AddNewBill } from "./AddNewBill";
 import { defaultBill } from "./commonData";
@@ -37,6 +37,8 @@ describe("Add new bill component", () => {
 const editRow = () => {
   clickEditButton();
   fillDetails();
+  saveRow();
+  expect(rowTotal()).toBe("₹10,80,000");
 };
 
 const clickEditButton = () => {
@@ -46,9 +48,49 @@ const clickEditButton = () => {
 
 const fillDetails = () => {
   const row = screen.getAllByRole("row")[1];
+  selectType(row);
+  enterName(row);
+  enterWeight(row);
+  enterRate(row);
+  enterMakingCharges(row);
+  enterOtherCharges(row);
+};
+
+const selectType = (row: HTMLElement) => {
   const cells = within(row).getAllByRole("cell");
-
   userEvent.click(within(cells[0]).getByRole("button"));
-
   userEvent.click(screen.getByLabelText(/Gold/i));
+};
+
+const enterName = (row: HTMLElement) => {
+  const cells = within(row).getAllByRole("cell");
+  userEvent.type(within(cells[1]).getByRole("textbox"), "Item Name");
+};
+const enterWeight = (row: HTMLElement) => {
+  const cells = within(row).getAllByRole("cell");
+  userEvent.type(within(cells[2]).getByRole("textbox"), "200");
+};
+const enterRate = (row: HTMLElement) => {
+  const cells = within(row).getAllByRole("cell");
+  userEvent.type(within(cells[3]).getByRole("textbox"), "49000");
+};
+const enterMakingCharges = (row: HTMLElement) => {
+  const cells = within(row).getAllByRole("cell");
+  userEvent.type(within(cells[4]).getByRole("spinbutton"), "500");
+};
+const enterOtherCharges = (row: HTMLElement) => {
+  const cells = within(row).getAllByRole("cell");
+  userEvent.type(within(cells[5]).getByRole("spinbutton"), "0");
+};
+
+const rowTotal = () => {
+  const row = screen.getAllByRole("row")[1];
+  const cells = within(row).getAllByRole("cell");
+  return cells[6].innerHTML;
+};
+
+const saveRow = () => {
+  const row = screen.getAllByRole("row")[1];
+  const cells = within(row).getAllByRole("cell");
+  userEvent.click(within(cells[7]).getAllByRole("button")[0]);
 };
