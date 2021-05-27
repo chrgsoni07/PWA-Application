f554c8bcb648e0d882ae81bfee281ea584782a1a
Ankit Soni <ankitprahladsoni@gmail.com>
2021-05-26 21:19:15 -0700

    add types

--- src/api/__mocks__/index.ts
+++ src/api/__mocks__/index.ts
@@ -1,15 +1,27 @@
-export const getBills = async () => {
+import { Bill } from "component/Bills/types";
+import { CustomerType } from "component/Customers/types";
+import { RateType } from "component/Rates/types";
+
+export const getBills = async (): Promise<Bill[]> => {
   return [];
 };
 
-export const getCustomers = async () => {
+export const getCustomers = async (): Promise<CustomerType[]> => {
+  return [
+    {
+      id: "1",
+      name: "Ramadhir",
+      address: "kuda ke pache",
+      mobile: "9009009000",
+      place: "Keer kheda",
+    },
+  ];
+};
+
+export const getRates = async (): Promise<RateType[]> => {
   return [];
 };
 
-export const getRates = async () => {
-  return [];
-};
-
-export const save = async (collection: any, data: any) => {
-  return {};
+export const save = async <T>(collection: any, data: any): Promise<T> => {
+  return {} as T;
 };
