4dfc526438bb52d9b64c95c87e2dc6e88d8d1439
Ankit Soni <ankitprahladsoni@gmail.com>
2021-05-29 19:31:06 -0700

    cleanup

--- src/toasts/index.tsx
+++ src/toasts/index.tsx
@@ -20,42 +20,34 @@ export const useToast = () => useContext(ToastContext);
 export const ToastsProvider: FC = ({ children }) => {
   const toast = useRef<Toast>(null);
 
-  const toastSuccess = useCallback(
-    (detail: ReactNode) =>
-      toast.current?.show({
-        severity: "success",
-        summary: "Success Message",
-        detail,
-      }),
-    []
-  );
-  const toastError = useCallback(
-    (detail: ReactNode) =>
-      toast.current?.show({
-        severity: "error",
-        summary: "Error Message",
-        detail,
-      }),
-    []
-  );
-  const toastWarn = useCallback(
-    (detail: ReactNode) =>
-      toast.current?.show({
-        severity: "warn",
-        summary: "Warn Message",
-        detail,
-      }),
-    []
-  );
-  const toastInfo = useCallback(
-    (detail: ReactNode) =>
-      toast.current?.show({
-        severity: "info",
-        summary: "info Message",
-        detail,
-      }),
-    []
-  );
+  const toastSuccess = useCallback((detail: ReactNode) => {
+    toast.current?.show({
+      severity: "success",
+      summary: "Success Message",
+      detail,
+    });
+  }, []);
+  const toastError = useCallback((detail: ReactNode) => {
+    toast.current?.show({
+      severity: "error",
+      summary: "Error Message",
+      detail,
+    });
+  }, []);
+  const toastWarn = useCallback((detail: ReactNode) => {
+    toast.current?.show({
+      severity: "warn",
+      summary: "Warn Message",
+      detail,
+    });
+  }, []);
+  const toastInfo = useCallback((detail: ReactNode) => {
+    toast.current?.show({
+      severity: "info",
+      summary: "info Message",
+      detail,
+    });
+  }, []);
   return (
     <ToastContext.Provider
       value={{ toastSuccess, toastError, toastWarn, toastInfo }}
