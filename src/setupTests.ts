53cd7fc0d510da2b0035b3742fd8e4e7aac59871
Ankit Soni <ankitprahladsoni@gmail.com>
2021-05-30 14:02:02 -0700

    fix for typescript

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,5 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-require("snapshot-diff/extend-expect");
+import "snapshot-diff";
+import "snapshot-diff/extend-expect";
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
