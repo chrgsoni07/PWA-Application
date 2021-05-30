e8f380287db4823ebfd53f2b5eeddb627bf7adbb
Ankit Soni <ankitprahladsoni@gmail.com>
2021-05-30 16:25:43 -0700

    update tests

--- src/component/Bills/index.test.tsx
+++ src/component/Bills/index.test.tsx
@@ -1,9 +1,4 @@
-import {
-  render,
-  screen,
-  waitForElementToBeRemoved,
-  within,
-} from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { ToastsProvider } from "toasts";
 import Bills from ".";
@@ -11,7 +6,7 @@ import Bills from ".";
 jest.mock("api");
 
 describe("Bills", () => {
-  it("should render", async () => {
+  it("should render bills table", async () => {
     const { asFragment } = render(
       <ToastsProvider>
         <Bills />
@@ -22,21 +17,40 @@ describe("Bills", () => {
 
     const tableloaded = asFragment();
     expect(firstRender).toMatchDiffSnapshot(tableloaded);
+  });
+
+  it("should render edit bill", async () => {
+    const { asFragment } = render(
+      <ToastsProvider>
+        <Bills />
+      </ToastsProvider>
+    );
+    await screen.findByRole("button", { name: "editBill" });
+    const beforeEditBill = asFragment();
+
     userEvent.click(screen.getByRole("button", { name: "editBill" }));
 
     expect(screen.getByText("2020202020")).toBeInTheDocument();
+    expect(beforeEditBill).toMatchDiffSnapshot(asFragment());
+
     const newItemTab = screen.getByRole("tabpanel", { name: "New Item" });
     expect(within(newItemTab).getAllByRole("row")).toHaveLength(4);
     userEvent.click(screen.getByRole("button", { name: /close/i }));
-    // const dialogOpened = asFragment();
-    // expect(tableloaded).toMatchDiffSnapshot(dialogOpened);
-    await waitForElementToBeRemoved(() =>
-      screen.queryByRole("dialog", { name: /new bill/i })
+  });
+
+  it("shoud render view bill", async () => {
+    const { asFragment } = render(
+      <ToastsProvider>
+        <Bills />
+      </ToastsProvider>
     );
+    await screen.findByRole("button", { name: "editBill" });
+    const beforeViewBill = asFragment();
     userEvent.click(screen.getByRole("button", { name: /viewBill/i }));
     expect(
       screen.getByRole("dialog", { name: /r k jewellers jawad/i })
     ).toBeInTheDocument();
+    expect(beforeViewBill).toMatchDiffSnapshot(asFragment());
     userEvent.click(screen.getByRole("button", { name: /close/i }));
   });
 });
