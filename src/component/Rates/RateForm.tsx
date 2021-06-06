c1a809fde259159dfdc039ad4e845704f17cd7c9
Ankit Soni <ankitprahladsoni@gmail.com>
2021-06-05 17:44:02 -0700

    refactor rate form

---
+++ src/component/Rates/RateForm.tsx
@@ -0,0 +1,124 @@
+import { FormikErrors, useFormik } from "formik";
+import { InputNumber } from "primereact/inputnumber";
+import { classNames } from "primereact/utils";
+import { FC, useEffect } from "react";
+import { useToast } from "toasts";
+import { RateType } from "./types";
+import { edit, save } from "api";
+import { defaultNum } from "utils/number.utils";
+
+type RateFormProps = {
+  formData?: RateType;
+  onAdd: (data: RateType) => void;
+  onEdit: (data: RateType) => void;
+  hideDialog: () => void;
+};
+
+const RateForm: FC<RateFormProps> = ({
+  formData,
+  onAdd,
+  onEdit,
+  hideDialog,
+}) => {
+  const formik = useFormik<RateType>({
+    initialValues: {
+      id: "",
+      silverRate: 0,
+      goldRate: 0,
+      date: new Date(),
+    },
+    validate: (data) => {
+      let errors: FormikErrors<RateType> = {};
+
+      if (!data.goldRate) {
+        errors.goldRate = "gold rate is required.";
+      }
+
+      if (!data.silverRate) {
+        errors.silverRate = "silver rate is required.";
+      }
+      return errors;
+    },
+
+    onSubmit: (data) => {
+      saveOrUpdateRate(data);
+      formik.resetForm();
+    },
+  });
+
+  useEffect(() => {
+    formData && formik.setValues(formData);
+  }, [formData, formik]);
+
+  const isFormFieldValid = (name: keyof RateType) =>
+    !!(formik.touched[name] && formik.errors[name]);
+
+  const getFormErrorMessage = (name: keyof RateType) =>
+    isFormFieldValid(name) && (
+      <small className="p-error">{formik.errors[name]}</small>
+    );
+
+  const saveOrUpdateRate = (data: RateType) => {
+    if (data.id) {
+      editRateToFireStore(data);
+    } else {
+      saveRateToFireStore(data);
+    }
+    hideDialog();
+  };
+
+  const { toastError } = useToast();
+
+  const editRateToFireStore = async (data: RateType) => {
+    try {
+      await edit("goldSilverRates", data);
+      onAdd(data);
+    } catch (e) {
+      toastError("Error updating rates");
+    }
+  };
+
+  const saveRateToFireStore = async (data: RateType) => {
+    try {
+      const savedItem = await save("goldSilverRates", data);
+      onEdit(savedItem);
+    } catch (err) {
+      toastError("Error saving rates");
+    }
+  };
+
+  return (
+    <form onSubmit={formik.handleSubmit} className="p-fluid" id="rateForm">
+      <div className="p-field">
+        <label htmlFor="goldRate">Gold rate</label>
+        <InputNumber
+          id="goldRate"
+          name="goldRate"
+          value={defaultNum(formik.values.goldRate)}
+          onValueChange={formik.handleChange}
+          autoFocus
+          className={classNames({
+            "p-invalid": isFormFieldValid("goldRate"),
+          })}
+        />
+        {getFormErrorMessage("goldRate")}
+      </div>
+
+      <div className="p-field">
+        <label htmlFor="silverRate">Silver rate</label>
+        <InputNumber
+          id="silverRate"
+          name="silverRate"
+          value={defaultNum(formik.values.silverRate)}
+          onValueChange={formik.handleChange}
+          className={classNames({
+            "p-invalid": isFormFieldValid("silverRate"),
+          })}
+        />
+        {getFormErrorMessage("silverRate")}
+      </div>
+    </form>
+  );
+};
+
+export default RateForm;
--- src/component/Rates/index.tsx
+++ src/component/Rates/index.tsx
@@ -6,66 +6,25 @@ import { Card } from "primereact/card";
 import { Dialog } from "primereact/dialog";
 import React, { useState, useEffect } from "react";
 import { RateType } from "./types";
-import { deleteFromDB, edit, getRates, save } from "api";
+import { deleteFromDB, getRates } from "api";
 import { useToast } from "toasts";
+import RateForm from "./RateForm";
 import { updateList } from "utils/state.utils";
-import { FormikErrors, useFormik } from "formik";
-import { classNames } from "primereact/utils";
-import { InputNumber } from "primereact/inputnumber";
 
 const Rates = () => {
   const [rates, setRates] = useState<RateType[]>([]);
-
   const [showDialog, setShowDialog] = useState(false);
-
-  const { toastSuccess, toastError } = useToast();
+  const { toastSuccess } = useToast();
+  const [formData, setFormData] = useState<RateType | undefined>(undefined);
 
   const editSelectedRate = (rowData: any) => {
     setShowDialog(true);
-    formik.setValues(rowData);
+    setFormData(rowData);
   };
 
   const dateTemplate = (rowData: any) => {
     var date = new Date(rowData.date);
     return date.toLocaleDateString("en-In");
-  };
-
-  const formik = useFormik<RateType>({
-    initialValues: {
-      id: "",
-      silverRate: 0,
-      goldRate: 0,
-      date: new Date(),
-    },
-    validate: (data) => {
-      let errors: FormikErrors<RateType> = {};
-
-      if (!data.goldRate) {
-        errors.goldRate = "gold rate is required.";
-      }
-
-      if (!data.silverRate) {
-        errors.silverRate = "silver rate is required.";
-      }
-      return errors;
-    },
-
-    onSubmit: (data) => {
-      saveOrUpdateRate(data);
-      formik.resetForm();
-    },
-  });
-
-  const isFormFieldValid = (name: keyof RateType) => {
-    return !!(formik.touched[name] && formik.errors[name]);
-  };
-
-  const getFormErrorMessage = (name: keyof RateType) => {
-    return (
-      isFormFieldValid(name) && (
-        <small className="p-error">{formik.errors[name]}</small>
-      )
-    );
   };
 
   useEffect(() => {
@@ -113,37 +72,7 @@ const Rates = () => {
 
   const hideDialog = () => {
     setShowDialog(false);
-  };
-
-  const saveOrUpdateRate = (data: RateType) => {
-    if (data.id) {
-      editRateToFireStore(data);
-    } else {
-      saveRateToFireStore(data);
-    }
-    hideDialog();
-  };
-
-  const editRateToFireStore = (data: RateType) => {
-    edit("goldSilverRates", data)
-      .then(() => {
-        toastSuccess("rate successfully updated");
-        const newRates = updateList(rates, data);
-        setRates(newRates);
-      })
-      .catch(() => {
-        toastError("Error updating rates");
-      });
-  };
-
-  const saveRateToFireStore = async (data: RateType) => {
-    try {
-      const savedItem: RateType = await save("goldSilverRates", data);
-      toastSuccess("rate successfully saved");
-      setRates([...rates, savedItem]);
-    } catch (err) {
-      toastError("Error saving rates");
-    }
+    setFormData(undefined);
   };
 
   const itemDialogFooter = (
@@ -160,25 +89,6 @@ const Rates = () => {
 
   return (
     <Card>
-      <div style={{ height: 235 }}>
-        <iframe
-          title="current rates"
-          src="https://www.goldpriceindia.com/wmshare-wlifop-001.php"
-          style={{
-            position: "relative",
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            width: "100%",
-            height: "100%",
-            border: "none",
-            margin: 0,
-            padding: 0,
-            overflow: "hidden",
-          }}
-        />
-      </div>
       <div className="card">
         <Toolbar left={leftToolbarTemplate}></Toolbar>
         <DataTable
@@ -214,37 +124,19 @@ const Rates = () => {
         footer={itemDialogFooter}
         onHide={hideDialog}
       >
-        <form onSubmit={formik.handleSubmit} className="p-fluid" id="rateForm">
-          <div className="p-field">
-            <label htmlFor="goldRate">Gold rate</label>
-            <InputNumber
-              id="goldRate"
-              name="goldRate"
-              value={formik.values.goldRate}
-              onValueChange={formik.handleChange}
-              autoFocus
-              className={classNames({
-                "p-invalid": isFormFieldValid("goldRate"),
-              })}
-            />
-            {getFormErrorMessage("goldRate")}
-          </div>
-
-          <div className="p-field">
-            <label htmlFor="silverRate">Silver rate</label>
-            <InputNumber
-              id="silverRate"
-              name="silverRate"
-              value={formik.values.silverRate}
-              onValueChange={formik.handleChange}
-              autoFocus
-              className={classNames({
-                "p-invalid": isFormFieldValid("silverRate"),
-              })}
-            />
-            {getFormErrorMessage("silverRate")}
-          </div>
-        </form>
+        <RateForm
+          formData={formData}
+          onAdd={(data: RateType) => {
+            toastSuccess("rate successfully updated");
+            const newRates = updateList(rates, data);
+            setRates(newRates);
+          }}
+          onEdit={(data: RateType) => {
+            toastSuccess("rate successfully saved");
+            setRates([...rates, data]);
+          }}
+          hideDialog={hideDialog}
+        />
       </Dialog>
     </Card>
   );
