3ab0763d046fdb05fa11b755909242873b8f70c6
Ankit Soni <ankitprahladsoni@gmail.com>
2021-06-06 00:06:51 -0700

    cleanup

--- src/component/Items/index.tsx
+++ src/component/Items/index.tsx
@@ -4,21 +4,17 @@ import ItemsPanel from "./ItemsPanel";
 
 const Items = () => {
   return (
-    <>
-      <div className="card">
-        <TabView>
-          <TabPanel header="Gold Items">
-            <ItemsPanel category="goldItems" />
-          </TabPanel>
-          <TabPanel header="Silver Items">
-            <ItemsPanel category="silverItems" />
-          </TabPanel>
-          <TabPanel header="Fixed Price Items">
-            <ItemsPanel category="fixed" />
-          </TabPanel>
-        </TabView>
-      </div>
-    </>
+    <TabView>
+      <TabPanel header="Gold Items">
+        <ItemsPanel category="goldItems" />
+      </TabPanel>
+      <TabPanel header="Silver Items">
+        <ItemsPanel category="silverItems" />
+      </TabPanel>
+      <TabPanel header="Fixed Price Items">
+        <ItemsPanel category="fixed" />
+      </TabPanel>
+    </TabView>
   );
 };
 
