c0ac5f878e53997531255423f0db1d3f74f2d8c0
Ankit Soni <ankitprahladsoni@gmail.com>
2021-06-09 21:25:48 -0700

    add tests

---
+++ src/component/Bills/ViewBill/__tests__/index.test.tsx
@@ -0,0 +1,26 @@
+import { render } from "@testing-library/react";
+import { BillDetails } from "component/Bills/types";
+import ViewAmountDetails from "../ViewAmountDetails";
+
+const { asFragment } = render(
+  <ViewAmountDetails billDetail={{} as BillDetails} />
+);
+const base = asFragment();
+it.each`
+  billDetail
+  ${null}
+  ${{}}
+`("should match snapshot for empty $billDetail", ({ billDetail }) => {
+  const { asFragment } = render(<ViewAmountDetails billDetail={billDetail} />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it.each`
+  billDetail
+  ${{ newTotal: 10, paid: 5, due: 5 }}
+  ${{ oldTotal: 10, paid: 5, due: 5 }}
+  ${{ newTotal: 10, oldTotal: 10, oldNewDifference: 0, paid: 5, due: 5 }}
+`("should match snapshot for $billDetail", ({ billDetail }) => {
+  const { asFragment } = render(<ViewAmountDetails billDetail={billDetail} />);
+  expect(asFragment()).toMatchDiffSnapshot(base);
+});
